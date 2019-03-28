f7146813c96159f7bdadbb3b4bfd1d388d33e09c
Jorge Macias <jorgemacias@hotmail.com>
2019-03-27 19:09:02 -0600

    Remove comments

--- src/Map.js
+++ src/Map.js
@@ -26,10 +26,9 @@ class MainMap extends Component {
       var longitude=this.props.positions[i].longitude
       console.log('Imei',imei)
       console.log('Azimuth',azimuth)
-
-    const coordinates = [latitude, longitude];
+      const coordinates = [latitude, longitude];
       children.push(<Popup>{imei}</Popup>)
-    table.push(<Marker position={coordinates}>{children}</Marker>)
+      table.push(<Marker position={coordinates}>{children}</Marker>)
     }
 
     const perc = this.props.perc || 50;
@@ -39,24 +38,6 @@ class MainMap extends Component {
   }
   render() {
     const position = [this.state.lat, this.state.lng, this.state.course]
-    this.listData()
-    const markers = this.props.positions.map(position =>
-      <DivIcon key={position.id.toString()} position={{ lat: position.latitude, lng: position.longitude,course:position.course }} className="" iconSize={[50, 50]}>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 50 50">
- {this.listData()}
-        </svg>
-
-      </DivIcon>
-    );
-        const markers2 = this.props.positions.map(position =>
-      <DivIcon key={position.id.toString()} position={{ lat: position.latitude, lng: position.longitude,course:position.course }} className="" iconSize={[50, 50]}>
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 50 50">
-<image x="13" y="13" fill="#fff" href="/images/arrow.svg" />
-        </svg>
-
-      </DivIcon>
-    );
-
     return (
       <Map style={{height: this.props.height, width: this.props.width}} center={position} zoom={this.state.zoom}>
         <TileLayer
